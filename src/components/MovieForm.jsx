843619ea560f83c97fd8c729ac3f76e8b741d8cb
fdureux <fanny.dureux@gmail.com>
2020-10-26 10:26:37 +0100

    add component MovieForm

---
+++ src/components/MovieForm.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./MovieForm.css";
+
+class MovieForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      poster: "",
+      comment: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+  submitForm(e) {
+    e.preventDefault();
+    const url = "https://post-a-form.herokuapp.com/api/movies/";
+    axios
+      .post(url, this.state)
+      .then((res) => res.data)
+      .then((res) => {
+        alert(`Movie added with the ID ${res.id} !`);
+      })
+      .catch((e) => {
+        console.error(e);
+        alert(
+          `Error when the movie was added : ${e.message} Please fill in all the fields!`
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div className="MovieForm">
+        <h1>Add a new film :</h1>
+        <form onSubmit={this.submitForm}>
+          <fieldset>
+            <legend>Informations</legend>
+            <div className="form-data">
+              <label htmlFor="title">Title : </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                onChange={this.onChange}
+                value={this.state.lastname}
+              />
+            </div>
+            <div className="form-data">
+              <label htmlFor="poster">Poster URL :</label>
+              <input
+                type="url"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                value={this.state.poster}
+              />
+            </div>
+            <div className="form-data">
+              <label htmlFor="comment">Comment :</label>
+              <input
+                type="comment"
+                id="comment"
+                name="comment"
+                onChange={this.onChange}
+                value={this.state.comment}
+              />
+            </div>
+            <hr />
+            <div className="form-data">
+              <input type="submit" value="Add !" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MovieForm;
